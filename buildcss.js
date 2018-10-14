6feb43934f2782a207311d58fa93b35b9facf085
ackman678 <james.ackman@gmail.com>
2018-10-14 12:21:47 -0700

    less batch

--- buildcss.js
+++ buildcss.js
@@ -1,19 +1,29 @@
 // npm install less
 // npm install less-plugin-clean-css
 // ./node_modules/less/bin/lessc --rootpath=/css/build/ --relative-urls --clean-css="--s1" build/bootstrap.less build/main.min.css
+// const buildcss = require('buildcss')
+// buildcss(srcPath, dstPath, overwrite, extFilter, options)
+// buildcss('_posts', '_site', true)
 
 const less = require('less')
 const fs = require('fs')
 const path = require('path')
 const CleanCSS = require('clean-css')
+const mm = require('micromatch')
 
 const options = {
-  filename: path.join(__dirname, 'build/bootstrap.less'),
+  srcPath: 'build/', 
+  dstPath: 'build/', 
+  overwrite: false, 
+  extFilter: ['*.less'],
+  // filename: path.join(__dirname, 'build/bootstrap.less'),
   // filename: path.join(__dirname, 'build/bootstrap-vita.less'),
-  rootpath: '/css/build/',
+  // outFile: path.join('build/', 'main.min.css'),
+  // outFile: path.join('build/', 'vita.css'),
+  // rootpath: '/css/build/',
   relativeUrls: true,
-  outFile: path.join('build/', 'main.min.css')
-  // outFile: path.join('build/', 'vita.css')
+  matchBase: false, 
+  ignore: ['.git', 'node_modules', 'README.md', 'test', 'lib', 'css', 'bin']
 }
 
 // getFile: reads utf8 content from a file
@@ -33,15 +43,36 @@ const writeFile = (fileName, datain) => new Promise((resolve, reject) => {
   })
 })
 
-readFile(options.filename)
-.then(str => { return less.render(str, options) })
-.then( data => {
-  new CleanCSS({level: 1, returnPromise: true})
-  .minify(data.css)
-  .then(output => {
-    writeFile(options.outFile, output.styles)
-    // console.log(data.imports)
+const readdir = (srcPath) => new Promise((resolve, reject) => {
+    fs.readdir(srcPath, (err, files) => {
+        if (err) reject(err)
+        else resolve(files)
+    })
+})
+
+const processFiles = (options) => {
+  //async process chain begin
+  readdir(options.srcPath)
+  .then(allfiles => { return mm(allfiles,options.extFilter,options) })
+  .then(files => {
+    files.forEach(file => {
+      
+      readFile(file)
+      .then(str => { return less.render(str, options) })
+      .then( data => {
+        new CleanCSS({level: 1, returnPromise: true})
+        .minify(data.css)
+        .then(output => {
+          writeFile(path.join(path.parse(file).name,'.min.css'), output.styles)
+          // console.log(data.imports)
+        })
+        .catch (err => { console.log(err) })
+      })
+      .catch (err => { console.log(err) })
+
+    })
   })
   .catch (err => { console.log(err) })
-})
-.catch (err => { console.log(err) })
+}
+
+processFiles(options)
